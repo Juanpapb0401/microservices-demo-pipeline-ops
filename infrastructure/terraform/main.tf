a677bf837f310c6c91bb47c8d5f851c6ffeac7fd
e5jparra1 <e5jparra@gmail.com>
2026-04-08 08:51:00 -0500

    feat: bulkhead better

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -48,6 +48,12 @@ resource "azurerm_kubernetes_cluster" "aks" {
     os_disk_size_gb = var.os_disk_size_gb
   }
 
+  network_profile {
+    network_plugin    = "azure"
+    network_policy    = "azure"
+    load_balancer_sku = "standard"
+  }
+
   identity {
     type = "SystemAssigned"
   }
